a031520fb0bb2bad007fccba65f2ccfe82a8712b
Stephen Liles <shinobiaoshi@gmail.com>
2015-07-28 00:20:47 -0500

    get mock data to display correctly in layout for artist search

--- app/src/main/java/com/spotify/sdliles/spotifystreamer/ArtistSearchFragment.java
+++ app/src/main/java/com/spotify/sdliles/spotifystreamer/ArtistSearchFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -25,7 +26,7 @@ public class ArtistSearchFragment extends Fragment
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_artist_search, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_artist_search, container, false);
         EditText artistSearchEditText = (EditText) rootView.findViewById(
                 R.id.artist_search_edit_text);
         artistSearchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener()
@@ -35,12 +36,27 @@ public class ArtistSearchFragment extends Fragment
             {
                 if(actionId == EditorInfo.IME_ACTION_SEARCH)
                 {
-                    // Call Spotify API
+                    ArtistAdapter artistAdapter = mockData();
+                    ListView artistList = (ListView) rootView.findViewById(R.id.artist_search_list_view);
+                    artistList.setAdapter(artistAdapter);
                     return true;
                 }
                 return false;
             }
         });
         return rootView;
+    }
+
+    private ArtistAdapter mockData()
+    {
+        Artist[] artists = new Artist[6];
+        artists[0] = new Artist("Linkin Park", "http://i.imgur.com/UbQK3Ww.jpg");
+        artists[1] = new Artist("Linkin Park 2", "http://i.imgur.com/UbQK3Ww.jpg");
+        artists[2] = new Artist("Linkin Park 3", "http://i.imgur.com/UbQK3Ww.jpg");
+        artists[3] = new Artist("Linkin Park 4", "http://i.imgur.com/UbQK3Ww.jpg");
+        artists[4] = new Artist("Linkin Park 5", "http://i.imgur.com/UbQK3Ww.jpg");
+        artists[5] = new Artist("Linkin Park 6", "http://i.imgur.com/UbQK3Ww.jpg");
+
+        return new ArtistAdapter(getActivity(), R.layout.list_artist_search_result, artists);
     }
 }
