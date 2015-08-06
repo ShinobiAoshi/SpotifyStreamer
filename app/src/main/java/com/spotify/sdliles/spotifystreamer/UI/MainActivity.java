df8363519d64e42850059606f4f42df19283b708
Stephen Liles <shinobiaoshi@gmail.com>
2015-08-06 01:32:10 -0500

    add Toolbar

--- app/src/main/java/com/spotify/sdliles/spotifystreamer/UI/MainActivity.java
+++ app/src/main/java/com/spotify/sdliles/spotifystreamer/UI/MainActivity.java
@@ -2,6 +2,7 @@ package com.spotify.sdliles.spotifystreamer.UI;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,10 +11,14 @@ import com.spotify.sdliles.spotifystreamer.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.activity_main_layout, new ArtistSearchFragment())
